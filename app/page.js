0d775be469a3ee8b56c932644f3b4d0e2025b2dd
dean0004 <dean_0004@hotmail.com>
2026-09-08 12:43:54 +1000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,9 +4,7 @@ import dynamic from "next/dynamic";
 
 const OutageMap = dynamic(
   () => import("./components/OutageMap"),
-  {
-    ssr: false,
-  }
+  { ssr: false }
 );
 
 export default function Home() {
@@ -16,15 +14,19 @@ export default function Home() {
         style={{
           background: "#711f32",
           color: "white",
-          padding: "30px",
+          padding: "30px"
         }}
       >
         <h1>Generator Rental Australia</h1>
         <p>Victoria Power Outage Map</p>
       </div>
 
-      <div style={{ padding: "20px" }}>
-        <h2>Melbourne Outage Map</h2>
+      <div
+        style={{
+          padding: "20px"
+        }}
+      >
+        <h2>Current Power Outages</h2>
 
         <OutageMap />
       </div>
