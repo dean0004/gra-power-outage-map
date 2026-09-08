b62a1c718c3ebd09584aed0c290ec1116984f3a1
dean0004 <dean_0004@hotmail.com>
2026-09-08 11:55:42 +1000

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "GRA Power Outage Map",
+  description: "Victoria Power Outage Map - Generator Rental Australia",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
