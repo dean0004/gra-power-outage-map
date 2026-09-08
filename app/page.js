58ec34d0b4e464ff3450115ccfdcc352bd93e4a9
dean0004 <dean_0004@hotmail.com>
2026-09-08 11:57:00 +1000

    Add test homepage

---
+++ app/page.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div style={{
+      padding: "50px",
+      fontFamily: "Arial"
+    }}>
+      <h1>Generator Rental Australia</h1>
+
+      <h2>Victoria Power Outage Map</h2>
+
+      <p>
+        If you can see this page, your Next.js deployment is working.
+      </p>
+    </div>
+  );
+}
