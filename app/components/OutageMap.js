5710c680c91bad08f4b07fc49cf52015095b3cb7
dean0004 <dean_0004@hotmail.com>
2026-09-08 12:43:29 +1000

    Update OutageMap.js

--- app/components/OutageMap.js
+++ app/components/OutageMap.js
@@ -3,25 +3,70 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
+const outages = [
+  {
+    suburb: "Melbourne CBD",
+    lat: -37.8136,
+    lng: 144.9631,
+    customers: 142
+  },
+  {
+    suburb: "Richmond",
+    lat: -37.8237,
+    lng: 145.0016,
+    customers: 53
+  },
+  {
+    suburb: "Essendon",
+    lat: -37.7567,
+    lng: 144.9167,
+    customers: 88
+  },
+  {
+    suburb: "Dandenong",
+    lat: -37.9875,
+    lng: 145.2150,
+    customers: 201
+  }
+];
+
 export default function OutageMap() {
   return (
-    <div style={{ height: "600px", width: "100%" }}>
+    <div style={{ height: "650px", width: "100%" }}>
       <MapContainer
         center={[-37.8136, 144.9631]}
         zoom={10}
         style={{ height: "100%", width: "100%" }}
       >
         <TileLayer
-          attribution="&copy; OpenStreetMap contributors"
+          attribution="© OpenStreetMap contributors"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
-        <Marker position={[-37.8136, 144.9631]}>
-          <Popup>
-            Melbourne CBD<br />
-            Sample outage marker
-          </Popup>
-        </Marker>
+        {outages.map((outage, index) => (
+          <Marker
+            key={index}
+            position={[outage.lat, outage.lng]}
+          >
+            <Popup>
+              <strong>{outage.suburb}</strong>
+              <br />
+              Customers affected: {outage.customers}
+              <br /><br />
+              <button
+                style={{
+                  background: "#711f32",
+                  color: "white",
+                  border: "none",
+                  padding: "8px 12px",
+                  cursor: "pointer"
+                }}
+              >
+                Request Generator
+              </button>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
