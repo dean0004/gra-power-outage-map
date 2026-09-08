e1a466e45d34b6baa370d3dd6bba771848328577
dean0004 <dean_0004@hotmail.com>
2026-09-08 12:54:03 +1000

    Add street and satellite map selector

--- app/components/OutageMap.js
+++ app/components/OutageMap.js
@@ -1,6 +1,13 @@
 "use client";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  LayersControl
+} from "react-leaflet";
+
 import "leaflet/dist/leaflet.css";
 
 const outages = [
@@ -8,40 +15,65 @@ const outages = [
     suburb: "Melbourne CBD",
     lat: -37.8136,
     lng: 144.9631,
-    customers: 142
+    customers: 142,
+    type: "Unplanned"
   },
   {
     suburb: "Richmond",
     lat: -37.8237,
     lng: 145.0016,
-    customers: 53
+    customers: 53,
+    type: "Planned"
   },
   {
     suburb: "Essendon",
     lat: -37.7567,
     lng: 144.9167,
-    customers: 88
+    customers: 88,
+    type: "Unplanned"
   },
   {
     suburb: "Dandenong",
     lat: -37.9875,
-    lng: 145.2150,
-    customers: 201
+    lng: 145.215,
+    customers: 201,
+    type: "Planned"
   }
 ];
 
 export default function OutageMap() {
   return (
-    <div style={{ height: "650px", width: "100%" }}>
+    <div
+      style={{
+        height: "100%",
+        minHeight: "650px",
+        width: "100%"
+      }}
+    >
       <MapContainer
         center={[-37.8136, 144.9631]}
         zoom={10}
-        style={{ height: "100%", width: "100%" }}
+        style={{
+          height: "100%",
+          minHeight: "650px",
+          width: "100%"
+        }}
       >
-        <TileLayer
-          attribution="© OpenStreetMap contributors"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+        <LayersControl position="topright">
+          <LayersControl.BaseLayer checked name="Street Map">
+            <TileLayer
+              attribution="&copy; OpenStreetMap contributors"
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+
+          <LayersControl.BaseLayer name="Satellite">
+            <TileLayer
+              attribution="Tiles &copy; Esri"
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+            />
+          </LayersControl.BaseLayer>
+        </LayersControl>
 
         {outages.map((outage, index) => (
           <Marker
@@ -49,21 +81,40 @@ export default function OutageMap() {
             position={[outage.lat, outage.lng]}
           >
             <Popup>
-              <strong>{outage.suburb}</strong>
-              <br />
-              Customers affected: {outage.customers}
-              <br /><br />
-              <button
+              <div
                 style={{
-                  background: "#711f32",
-                  color: "white",
-                  border: "none",
-                  padding: "8px 12px",
-                  cursor: "pointer"
+                  minWidth: "190px",
+                  fontFamily: "Arial, sans-serif"
                 }}
               >
-                Request Generator
-              </button>
+                <strong
+                  style={{
+                    fontSize: "16px"
+                  }}
+                >
+                  {outage.suburb}
+                </strong>
+
+                <p
+                  style={{
+                    margin: "8px 0 4px"
+                  }}
+                >
+                  {outage.type} outage
+                </p>
+
+                <p
+                  style={{
+                    margin: "4px 0 12px"
+                  }}
+                >
+                  Customers affected: {outage.customers}
+                </p>
+
+                tel:0393698800
+                  Call GRA
+                </a>
+              </div>
             </Popup>
           </Marker>
         ))}
