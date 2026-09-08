b2aeed096b2daf61af54bfc253c9cfae8814a906
dean0004 <dean_0004@hotmail.com>
2026-09-08 12:34:52 +1000

    Update OutageMap.js

--- app/components/OutageMap.js
+++ app/components/OutageMap.js
@@ -1,1 +1,28 @@
+"use client";
 
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+export default function OutageMap() {
+  return (
+    <div style={{ height: "600px", width: "100%" }}>
+      <MapContainer
+        center={[-37.8136, 144.9631]}
+        zoom={10}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          attribution="&copy; OpenStreetMap contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        <Marker position={[-37.8136, 144.9631]}>
+          <Popup>
+            Melbourne CBD<br />
+            Sample outage marker
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+}
